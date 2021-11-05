a354f29b8e21b060bbc9198b70e54e6af6ee56fb
James Scott <tlmsols50@gmail.com>
2021-11-04 20:48:33 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/*
+ * Primary file for API
+ *
+ */
+
+// Dependencies
+const server = require("./lib/server");
+const workers = require("./lib/workers");
+
+// Declare the app
+const app = {};
+
+// Init function
+app.init = function () {
+  // Start the server
+  server.init();
+
+  // Start the workers
+  workers.init();
+};
+
+// Self executing
+app.init();
+
+// Export the app
+module.exports = app;
